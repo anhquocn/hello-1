10f4bc593da119e41ee012c25cee5bc903588121
anhquocn <54682359+anhquocn@users.noreply.github.com>
2019-08-29 12:56:49 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Anh Nguyen & Aaron Downs 
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Anh & Aaron\n");
+
+  return 0;
+}
